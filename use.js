ac6a254738ad3b56dadbd13ece62a29c61ebd727
actinide118 <titouan.actinide@proton.me>
2025-10-06 09:19:03 +0200

    implentation du système "arborescence de dossiers"

--- use.js
+++ use.js
@@ -1,16 +1,20 @@
 const {Suite} = require("./proto1");
 var AdmZip = require("adm-zip");
+const {writeFileSync,mkdirSync, mkdir} = require("node:fs")
+const path = require("node:path");
 let zip = new AdmZip();
-let len =25
+let len =10
 let refarr = [];
 function writechildren(obj, path,itt=0){
   if(obj.children !== undefined){
     obj.children.forEach(element => {
+      mkdirSync(path+"/"+`[${itt}]${obj.isplayer1 ? "":"n"}${obj.number}`+`(${obj.min_max_value})`, { recursive: true });
       writechildren(element, path+"/"+`[${itt}]${obj.isplayer1 ? "":"n"}${obj.number}`+`(${obj.min_max_value})`,itt+1);
     });
   }else{
     //zip.addFile(path+"/"+obj.number+".json", Buffer.from(JSON.stringify(obj), "utf8"), "entry comment goes here");
-    zip.addFile(path+"/"+`[${itt}]${obj.isplayer1 ? "":"n"}${obj.number} (${obj.min_max_value})-> FINNISH PLAYER ${obj.finnish}\n`, Buffer.from(JSON.stringify(obj), "utf8"), "entry comment goes here")
+    //zip.addFile(path+"/"+`[${itt}]${obj.isplayer1 ? "":"n"}${obj.number} (${obj.min_max_value})-> FINNISH PLAYER ${obj.finnish}\n`, Buffer.from(JSON.stringify(obj), "utf8"), "entry comment goes here")
+    writeFileSync(path+"/"+`[${itt}]${obj.isplayer1 ? "":"n"}${obj.number} (${obj.min_max_value})-> FINNISH PLAYER ${obj.finnish}\n`, Buffer.from(JSON.stringify(obj), "utf8"))
   }
 }
 for (let i = 1; i < len+1; i++){
@@ -22,9 +26,9 @@ for (let i = 2;i <=len; i+=2){
     let s = new Suite(line,true,i);
     
     let obj = s.toObject();
-    writechildren(obj, `/`);
+    writechildren(obj, path.resolve(__dirname, `table${len}`));
     //console.log(s.toString());
     
 
 }
-zip.writeZip( `./table${len}.zip`);
+//zip.writeZip( `./table${len}.zip`);
