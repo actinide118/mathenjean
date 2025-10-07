d90802c99d9bf8d1a3e4a57ef12c52da07d2d2c7
actinide118 <titouan.actinide@proton.me>
2025-10-07 21:20:11 +0200

    Début de l'implémentation d'une fonctionnalité d'application interactive avec la création du début de son coeur

---
+++ app/core.js
@@ -0,0 +1,64 @@
+const {Suite} = require("../proto1");
+
+
+class CoreInterface {
+  
+  constructor(){
+    this.all_suites = []
+  }
+  /**
+   * 
+   * @param {Number} nombre 
+   */
+  generate_suite(nombre){
+    let refarr = [];
+    let suite = []
+    for (let i = 1; i < nombre+1; i++){
+      refarr.push(i);
+    }
+    for (let i = 2;i <=nombre; i+=2){
+      let line = refarr.filter((e) => e !== i);
+
+      let s = new Suite(line,true,i);
+    
+      let obj = s.toObject();
+      suite.push(obj);
+    }
+    this.all_suites.push(suite);
+  }
+  /**
+   * 
+   * @param {Number} suite_id
+   * @param {Array} deroulement 
+   */
+  get_after(suite_id,deroulement){
+    if (this.all_suites[suite_id] === undefined) {
+      throw new Error("Suite not found");
+    }
+    let suite = this.all_suites[suite_id];
+    if(deroulement.length == 0){
+      return suite.map(
+        (e)=>{
+        return {min_max_value: e.min_max_value, number: e.number, isplayer1: e.isplayer1}
+      })
+    }
+    try{
+    deroulement.forEach(
+      (el)=>{
+        suite.forEach((elem)=>{
+          if(elem.number == el){
+            suite = elem.children
+          }
+        })
+    })
+    return suite.map(
+              (e)=>{
+                return {min_max_value: e.min_max_value, number: e.number, isplayer1: e.isplayer1}
+              })
+    }catch(e){
+      throw e
+    }
+  }
+}
+
+module.exports.CoreInterface = CoreInterface;
