fb8ae25f683077b7ace279af31277d32aba9e036
actinide118 <titouan.actinide@proton.me>
2025-11-16 22:17:59 +0100

    Mise au point de la structure mémoire et debut du développement de la structure logique

--- proto2.c
+++ proto2.c
@@ -1,11 +1,13 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "compile_options.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct suite suite;
 struct suite{
   uint8_t number;
-  suite* parent;
+  suite *parent;
   bool game_ended;
   bool is_min_max_def;
   bool min_max_val;
@@ -13,5 +15,36 @@ struct suite{
   uint8_t number_of_children;
   suite* children_array_pointer[COMPILE_MAX_CHILDREN];
 };
+suite global_temporary_array[COMPILE_MAX_CHILDREN];
+//suite* sut;// = (suite*)malloc(sizeof(struct suite));
 
-void main(){}
+suite* generate_suite(uint8_t number, suite* parent){
+  suite* generated_suite = (suite*)malloc(sizeof(struct suite));
+  generated_suite->number=number;
+  generated_suite->is_min_max_def=false;
+  generated_suite->min_max_val=false;
+  generated_suite->is_processed=false;
+  generated_suite->parent=parent;
+  return generated_suite;
+}
+
+int main(){
+  suite* null_holder = (suite*)malloc(sizeof(struct suite));
+  suite* sut= (suite*)malloc(sizeof(struct suite));
+  sut = generate_suite((uint8_t)6,null_holder); 
+
+start:
+
+routine:
+
+crawl:
+
+defminmax:
+
+memory_allocation_fail:
+  printf("System does not want us to use memory");
+  return 1;
+
+everythings_fine:
+  return 0;
+}
