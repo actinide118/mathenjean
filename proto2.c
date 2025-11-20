f3e9206d24cccc7f1a12e4cc3b351008a68d9b7f
actinide118 <titouan.actinide@proton.me>
2025-11-20 20:06:07 +0100

    Implémentation de toute la logique TODO: exploitation de données; vérification du min max

--- proto2.c
+++ proto2.c
@@ -12,8 +12,10 @@ struct suite{
   bool is_min_max_def;
   bool min_max_val;
   bool is_processed;
+  bool isplayer1;
   uint8_t number_of_children;
-  suite* children_array_pointer[COMPILE_MAX_CHILDREN];
+  suite** children_array_pointer;
+  //suite* children_array_pointer[COMPILE_MAX_CHILDREN];
 };
 suite global_temporary_array[COMPILE_MAX_CHILDREN];
 uint8_t base_cards[NUMBER_OF_CARDS];
@@ -28,8 +30,11 @@ suite* temporary_suite_pointer;
 uint8_t len;
 uint8_t len2;
 uint8_t tab_putter;
+suite* current_suite;
+bool is_all_children_defined;
+bool min_max_curr_val;
 
-suite* generate_suite(uint8_t number, suite* parent){
+suite* generate_suite(uint8_t number, suite* parent, bool isplayer1){
   suite* generated_suite = (suite*)malloc(sizeof(struct suite));
   if (generated_suite == NULL){
     return NULL;
@@ -39,6 +44,8 @@ suite* generate_suite(uint8_t number, suite* parent){
   generated_suite->min_max_val=false;
   generated_suite->is_processed=false;
   generated_suite->parent=parent;
+  generated_suite->isplayer1 = isplayer1;
+  generated_suite->children_array_pointer = NULL;
   return generated_suite;
 }
 
@@ -64,9 +71,9 @@ void init_cards(){
 uint8_t playable_card(suite* processed_suite){
   if (processed_suite->number == 0){
     currentcard_generator = 2;
-    loop_counter= 1;
+    loop_counter= 0;
     while(  currentcard_generator <= NUMBER_OF_CARDS){
-      playable_cards[loop_counter-1]=currentcard_generator;
+      playable_cards[loop_counter]=currentcard_generator;
       currentcard_generator+=2;
       loop_counter ++;
     }
@@ -82,13 +89,19 @@ uint8_t playable_card(suite* processed_suite){
     while(loop_counter<NUMBER_OF_CARDS){
       if(!temporary_suite_pointer->number==0){
         already_played_cards[loop_counter]=temporary_suite_pointer->number;
-        temporary_suite_pointer->parent;
+        temporary_suite_pointer=temporary_suite_pointer->parent;
         loop_counter++;
       }else{
         break;
       }
     }
     len=loop_counter;
+    loop_counter=0;
+    printf("already played cards: ");
+    while (loop_counter<len){
+      printf("%d; ",already_played_cards[loop_counter]);
+      loop_counter++;
+    }
     loop_counter=2;
     len2 = 0;
     tab_putter=0;
@@ -109,31 +122,102 @@ uint8_t playable_card(suite* processed_suite){
 }
 
 int suite_manager(){
-  suite* null_holder = generate_suite((uint8_t)0,0);
+  suite* null_holder = generate_suite((uint8_t)0,0,false);
   if(null_holder == NULL){
+    printf("here");
     goto memory_allocation_fail;
   }
-  suite* sut = generate_suite((uint8_t)4,null_holder);
-  playable_cards_counter= playable_card(sut);
-  printf("number of cards:%d\n",playable_cards_counter);
-  printf("%d\n",playable_cards[0]);
-  printf("%d\n",playable_cards[1]);
-  printf("%d\n",playable_cards[2]);
-  printf("%d\n",playable_cards[3]);
+  current_suite = null_holder;
   init_cards();
-  goto everythings_fine;
 
 routine:
+  printf("routine entry: %d\n",current_suite->number);
+  playable_cards_counter = playable_card(current_suite);
+  if(playable_cards_counter==0){
+    current_suite->is_min_max_def=true;
+    current_suite->game_ended = true;
+    current_suite->min_max_val= !current_suite->isplayer1;
+    current_suite->is_processed=true;
+    current_suite=current_suite->parent;
+    printf("game ended\n");
+    goto defminmax;
+  }
+  loop_counter=0;
+  suite** arr = (suite**)malloc(playable_cards_counter * sizeof(suite*));
+  if (arr == NULL) {
+    goto memory_allocation_fail;
+  } 
+  printf("current card:%d\n", current_suite->number);
+  while (loop_counter < playable_cards_counter) {
+    arr[loop_counter] = generate_suite(playable_cards[loop_counter], current_suite,!current_suite->isplayer1);
+    printf("card: %d\n",playable_cards[loop_counter]);
+    if (arr[loop_counter] == NULL) {
+      goto memory_allocation_fail;
+    }
+    loop_counter++;
+  }
+
+  current_suite->children_array_pointer = arr;
+  current_suite->number_of_children = playable_cards_counter;
+  current_suite->is_processed=true;
+  goto crawl;
 
 crawl:
+  printf("crawl entry: %d<-",current_suite->number);
+  if(current_suite->number==0){
+    printf("\n");
+  }else{
+    printf("%d\n",current_suite->parent->number);
+  }
+
+  loop_counter=0;
+   while(loop_counter<current_suite->number_of_children){
+    if(!current_suite->children_array_pointer[loop_counter]->is_processed){
+      current_suite=current_suite->children_array_pointer[loop_counter];
+      goto routine;
+    }
+    loop_counter++;
+  }
 
 defminmax:
+  printf("defminmax entry: %d<-",current_suite->number);
+  if(current_suite->number==0){
+    printf("\n");
+  }else{
+    printf("%d\n",current_suite->parent->number);
+  }
+  loop_counter=0;
+  is_all_children_defined=true;
+  min_max_curr_val=current_suite->isplayer1;
+  while(loop_counter<current_suite->number_of_children){
+    if(!current_suite->children_array_pointer[loop_counter]->is_min_max_def){
+      goto crawl;
+    }
+    if(current_suite->isplayer1 && !current_suite->children_array_pointer[loop_counter]->min_max_val){
+      min_max_curr_val=false;
+    }else if(current_suite->children_array_pointer[loop_counter]->min_max_val){
+      min_max_curr_val=true;
+    }
+    loop_counter++;
+  }
+  current_suite->is_min_max_def= true;
+  current_suite->min_max_val=min_max_curr_val;
+   if(current_suite->number==0){
+    goto everythings_fine;
+  }
+  current_suite=current_suite->parent;
+
+  goto defminmax;
 
 memory_allocation_fail:
   printf("System does not want us to use memory");
   return 1;
 
 everythings_fine:
+  printf("Number of children: %d\n", current_suite->number_of_children);
+  for (int i = 0; i < current_suite->number_of_children; i++) {
+    printf("Child %d number: %d\n", i, current_suite->children_array_pointer[i]->number);
+  }
   return 0;
 }
 
